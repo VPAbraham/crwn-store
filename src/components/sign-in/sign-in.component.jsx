bfe3ff90899c7f8fbe165c35f87c4e7ee9ffd57d
Victor Abraham <vpa4565@gmail.com>
2021-01-31 16:07:15 -0700

    Create a custom button component to be resued with similar styling throughout the app

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
-
+import CustomButton from '../../components/custom-button/custom-button.component';
 
 class SignIn extends React.Component{
   constructor(props) {
@@ -45,7 +45,7 @@ class SignIn extends React.Component{
             label='password'
             required
             />
-            <input type='submit' value='Submit Form' />
+            <CustomButton type='submit'> Sign In </CustomButton>
           </form>
         </div>
       )
