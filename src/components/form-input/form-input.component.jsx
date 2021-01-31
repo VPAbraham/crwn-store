881ee4606fd6ec092fa8e6a2804a169c72c46522
Victor Abraham <vpa4565@gmail.com>
2021-01-31 15:44:21 -0700

    Replace inputs on sign in with new form inputs and add styling and focus animations for labels

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './form-input.styles.scss';
 
-const FormInput = ({ handleChange, label, ...props }) => {
+const FormInput = ({ handleChange, label, ...otherProps }) => {
 
   return(
   <div className='group'>
     <input className='form-input' onChange={handleChange} {...otherProps} />
     {
       label ? 
-      (<label className={`${props.value.length ? 'shrink' : ''} form-input-label`}>
-
+      (<label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>
+        {label}
       </label>)
       : null
     }
